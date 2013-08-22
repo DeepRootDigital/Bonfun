8cdff8bf3f9625f9e8dc24bc98dfc2cb82e94591
Paul McMahon <colpanius@gmail.com>
2013-08-21 17:19:25 -0700

    Cleaned up a bit, added swipe and mobile stuff

--- function.js
+++ function.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
 
+	$('.pull').click(function() {
+		$('.nav-menu').toggle();
+	});
+
+	$('.process-button').click(function() {
+		if ($('.process-content').css('height') === "395px"){
+			$('.container').css("display","none");
+			$('.grid-area').animate({'margin-top':'-86px'},500);
+			$('.process-content').animate({"height":"0px"},500,function(){
+				$('.process-content').css("display","none");
+			});
+		}
+		else {
+			$('.process-content').css("display","block");
+			$('.process-content').animate({"height":"395px"},500);
+			$('.container').css("display","block");
+			$('.grid-area').css('margin-top','0px');
+		}
+		
+	});
+
+	$(".carousel").CloudCarousel(		
+	{			
+		xPos: 128,
+		yPos: 32,
+		xRadius: 52,
+		yRadius: 32,
+		autoRotate: 'left',
+		autoRotateDelay: 3000,
+		bringToFront: true,
+	});
+
 	$(".grid-cover").hover(
 		function(){
 			$(this).animate({"width":"100%"},500);	
@@ -25,4 +57,44 @@ $(document).ready(function(){
 			$(".grid-top").animate({"height":"640px"},1000);
 		}
 	});
+
+
+
+	// Process Mobile Slide
+	processsize();
+
+	window.onresize = function(event) {
+		processsize()
+	}
+
+	var slidenumber = 1;
+
+	function processsize() {
+		if ($(".process-viewport").width() < 850) {
+			var screenwidth = $(window).width();
+			var fivewidth = screenwidth * 6;
+			$(".process-panel-string").css('width',fivewidth + "px");
+			$('.process-panel').css('width',screenwidth+"px");
+			$('.process-panel-string').css('margin-left',"-" + ((slidenumber * screenwidth) - screenwidth) + "px");
+		}
+	}
+
+	$(".process-small-nav-left").click(function(){
+		var panelwidth = $('.process-panel').width();
+		slidenumber -= 1;
+
+		$('.process-panel-string').animate({'margin-left':'+='+panelwidth+'px'},500);
+	});
+
+	$(".process-small-nav-right").click(function(){
+		var panelwidth = $('.process-panel').width();
+		slidenumber += 1;
+
+		$('.process-panel-string').animate({'margin-left':'-='+panelwidth+'px'},500);
+	});
+
+	// Make content draggable
+	$( ".process-panel-string" ).draggable({ axis: "x" });
+
+
 });
